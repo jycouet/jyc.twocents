68bd2fcdf2b666f7c00fc137494236e410a34720
jycouet <jycouet@gmail.com>
2024-01-03 20:48:26 +0100

    :tada: NEW: init twocents

---
+++ src/lib/siteConfig.js
@@ -0,0 +1,18 @@
+export const SITE_URL = 'https://jyc.twocents.onrender.com';
+export const APPROVED_POSTERS_GH_USERNAME = ['jycouet'];
+export const GH_USER_REPO = 'jycouet/swyxkit'; // used for pulling github issues and offering comments
+export const SITE_TITLE = 'JYC TwoCents';
+export const SITE_DESCRIPTION = 'JYC TwoCents on this & that';
+export const DEFAULT_OG_IMAGE = 'https://avatars.githubusercontent.com/u/5312607?v=4';
+export const MY_TWITTER_HANDLE = 'jycouet';
+export const MY_YOUTUBE = 'https://youtube.com/JeanYvesCOUET';
+export const POST_CATEGORIES = ['Snippets', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+export const GH_PUBLISHED_TAGS = ['Published'];
+
+// auto generated variables
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const REPO_OWNER = GH_USER_REPO.split('/')[0];
+
+// dont forget process.env.GH_TOKEN
+// if supplied, raises rate limit from 60 to 5000
+// https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
